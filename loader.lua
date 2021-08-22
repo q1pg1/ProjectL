b44aee216851b62eee96ca61b71fdea51bdf90db
q1pg1 <72203639+q1pg1@users.noreply.github.com>
2021-08-22 18:41:19 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,29 @@
-game.Players.LocalPlayer:Kick("Jamarion hacked AND REKT LIKE FUCK REKT")
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.Position = UDim2.new(0.316885114, 0, 0.267484665, 0)
+Frame.Size = UDim2.new(0, 583, 0, 398)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(0.308747858, 0, 0.34673366, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "u know candice?"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
